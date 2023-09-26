ca5cf618514e686e6821393a9644887dfd9f583f
Maulik A <maulik.ankolia2@gmail.com>
2023-09-26 11:47:47 +0100

    Initial setup files

---
+++ rundate.py
@@ -0,0 +1,2 @@
+time_period_from = '2023-06-01T00:30Z'
+time_period_to = '2023-07-01T00:00Z'
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="project_green",
+    packages=find_packages(exclude=["project_green_tests"]),
+    install_requires=[
+        "dagster",
+        "pandas",
+        "psycopg",
+        "datetime"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
